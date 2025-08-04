fbdbf0051138477b13acddaf8504f2c65e2e78a7
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-08-04 10:04:46 +0200

    add new review form

--- src/pages/SingleMovie.jsx
+++ src/pages/SingleMovie.jsx
@@ -6,6 +6,7 @@ export default function SingleMovie() {
     const { id } = useParams()
     const movie_api_url = `${import.meta.env.VITE_API_URL}/${id}`
     const [singleMovie, setSingleMovie] = useState(null)
+    const [isClicked, setIsClicked] = useState(false)
 
     useEffect(() => {
         fetch(movie_api_url)
@@ -41,7 +42,8 @@ export default function SingleMovie() {
 
                                 <button
                                     type="button"
-                                    class="btn btn-warning"
+                                    className="btn btn-warning"
+                                    onClick={() => setIsClicked(true)}
                                 >
                                     Review
                                 </button>
@@ -52,8 +54,67 @@ export default function SingleMovie() {
                 </div>
                 <section className="container-fluid my-5">
                     <h2>Reviews</h2>
+                    {/* NEW REVIEW FORM */}
+                    <div className={isClicked ? "d-block" : "d-none"}>
+                        <div className="my-4 card p-3">
+                            <h3>Add your Review</h3>
+                            <form>
+                                <div className="row">
+                                    <div className="col-6">
+                                        <div className="mb-3">
+                                            <label htmlFor="" className="form-label">UserName</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                name=""
+                                                id=""
+                                                aria-describedby="helpId"
+                                                placeholder=""
+                                            />
+
+                                        </div>
+                                    </div>
+                                    <div className="col-6">
+                                        <div className="mb-3">
+                                            <label htmlFor="" className="form-label">Vote</label>
+                                            <input
+                                                type="number"
+                                                className="form-control"
+                                                name=""
+                                                id=""
+                                                aria-describedby="helpId"
+                                                placeholder=""
+                                            />
+
+                                        </div>
+                                    </div>
+                                    <div className="col-12">
+                                        <div className="mb-3">
+                                            <label htmlFor="exampleFormControlTextarea1" className="form-label">Review Text</label>
+                                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                        </div>
+                                    </div>
+                                    <div className="text-center">
+
+                                        <button
+                                            type="submit"
+                                            className="btn btn-primary"
+                                        >
+                                            Publish
+                                        </button>
+                                    </div>
+
+                                </div>
+
+                            </form>
+
+                        </div>
+                    </div>
+
+
+
                     <div className="row g-3" >
-                        {console.log(singleMovie?.reviews)}
+                        {console.log(singleMovie?.reviews, isClicked)}
                         {singleMovie?.reviews.map((element, index) => {
                             let updated = Date(element.updated_at)
                             return (
